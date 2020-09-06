dc1d21b6f922c323e678bf9e62d9c888a9e4cb41
saychakra <saychakra@gmail.com>
2020-09-06 21:03:18 +0530

    changing app.js to class based, and passing term from SearchBar to parent

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "20px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "20px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        {/* Note: here we could have given any name other than onSubmit unlike in generic jsx */}
+      </div>
+    );
+  }
+}
 
 export default App;
