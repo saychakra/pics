025224ce3f352bc95b9b71830bc2dcb64304e6f5
saychakra <saychakra@gmail.com>
2020-09-06 19:11:49 +0530

    refractored SearchBar.js from uncontrolled to controlled

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: "" };
 
   render() {
     return (
@@ -15,7 +13,11 @@ class SearchBar extends React.Component {
                   we DO NOT pass it like this.onInputChange() - which suggests that the function will be 
                   called everytime the render() function is rendered. Instead () is avoided to make sure 
                   that the reference to the callback function is given to <input> which would call it instead */}
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
             {/* here, onChange cannot be changed to any other name, the function onInputChange can be though */}
           </div>
         </form>
