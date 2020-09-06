54db6a3efec4776a9a5afa7556852c1ecdbbef6a
saychakra <saychakra@gmail.com>
2020-09-06 17:39:39 +0530

    added searchbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import SearchBar from "./SearchBar";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container" style={{ marginTop: "20px" }}>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default App;
