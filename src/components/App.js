108479609d402d70f8b7666992337b9ab5c6db96
saychakra <saychakra@gmail.com>
2020-09-07 21:29:22 +0530

    error commit showing how this can mess everything up

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  state = { images: [] };
   async onSearchSubmit(term) {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
@@ -11,13 +12,16 @@ class App extends React.Component {
       },
     });
 
-    console.log(response.data.results);
+    console.log(this);
+
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "20px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images.
         {/* Note: here we could have given any name other than onSubmit unlike in generic jsx */}
       </div>
     );
