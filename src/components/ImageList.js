d57e3ac3d6bb53f888fb7eafb329f8628ae03bb0
saychakra <saychakra@gmail.com>
2020-09-07 22:51:54 +0530

    added image id, alt image to remove react warnings

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-    return <img src={image.urls.regular} />;
+    // note: key should be added to the root property of any tag.
+    // so if we had a <div> wrapped around the <img/>, then we would be specifying the keys on the <div>
+    return (
+      <img key={image.id} src={image.urls.regular} alt={image.description} />
+      // we could have also used js destructuring, but it looked a bit tough for me!
+    );
   });
 
   return <div>{images}</div>;
