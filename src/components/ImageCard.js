49794ab6ed53439cb5303ffd521142b327a3506c
saychakra <saychakra@gmail.com>
2020-09-20 00:39:41 +0530

    added image reference and some important notes regarding sequence of control flow

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  //creating the reference for the image
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+
+  //console logging the entire application after the component gets rendered
+  componentDidMount() {
+    // here if we console log the imageRef, we will be able to see the clientHeight
+    // but console logging the clientHeight within this componentDidMount() is too early, because the
+    // image is not downloaded completely from the api source and before that the console log is taking place
+    console.log(this.imageRef.current.clientHeight);
+  }
+
   render() {
     // destructuring the reusable compoents for code readability
     const { description, urls } = this.props.image;
 
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
